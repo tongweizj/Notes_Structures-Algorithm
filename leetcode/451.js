0566b43269cf77ea6b637bf8aaf7314aed9b9c32
max <max@luci.com>
2023-03-30 23:39:45 -0400

    update

--- leetcode/451.js
+++ leetcode/451.js
@@ -9,25 +9,13 @@ var frequencySort = function (s) {
   for (let i = 0; i < s.length; i++) {
     map.set(s[i], map.get(s[i]) + 1 || 1);
   }
-  console.log(map);
+
   // 排序
-  map.forEach((value, key, map) => {
-    console.log(`${key}: ${value}`); // cucumber: 500 etc
-  });
-  map.forEach(function (value, key, map) {
-    console.log("1", key, value);
-    sort[value] = key;
-  });
-  console.log(sort);
+  sort = [...map].sort((a, b) => b[1] - a[1]);
 
   // 重组
-  let length = sort.length;
   let out = "";
-  for (let i = length - 1; i > 0; i--) {
-    for (let j = i; j < 0; j--) {
-      out.push(sort[j]);
-    }
-  }
+  for (let char of sort) out += char[0].repeat(char[1]);
   return out;
 };
 
