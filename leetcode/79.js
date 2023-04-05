f11ed02fba1c60928ded971e72ef03ad796b24af
max <max@luci.com>
2023-04-04 22:19:50 -0400

    update

--- leetcode/79.js
+++ leetcode/79.js
@@ -4,9 +4,34 @@
  * @return {boolean}
  */
 var exist = function (board, word) {
+  const dirs = [
+    [-1, 0],
+    [0, 1],
+    [1, 0],
+    [0, -1],
+  ];
+  const h = board.length;
+  const w = board[0].length;
+  var search = function (board, word, k, x, y) {
+    if (word[k] !== board[x][y]) return false;
+    if (k == word.length - 1) return true;
+
+    board[x][y] = "*";
+    for (const [dx, dy] of dirs) {
+      const i = x + dx;
+      const j = y + dy;
+      if (i >= 0 && i < h && j >= 0 && j < w) {
+        if (search(board, word, k + 1, i, j)) return true;
+      }
+    }
+
+    board[x][y] = word[k];
+
+    return false;
+  };
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[0].length; j++) {
-      let out = search(board, word, new Set(), 0, i, j);
+      let out = search(board, word, 0, i, j);
       if (out == true) return true;
     }
   }
@@ -14,35 +39,11 @@ var exist = function (board, word) {
   return false;
 };
 
-var search = function (board, word, path, k, x, y) {
-  console.log("path", path, "k", k, "x", x, "y", y);
-  if (k == word.length - 1) return true;
-  if (word[k] !== board[x][y]) return false;
-
-  if (word[k] == board[x][y]) {
-    path.add([x, y]);
-    if (y < board[0].length - 1 && path.has([x, y + 1]) == false) {
-      search(board, word, path, k + 1, x, y + 1);
-    }
-    if (x < board.length - 1 && !path.has([x + 1, y]) == false) {
-      search(board, word, path, k + 1, x + 1, y);
-    }
-    if (x > 0 && !path.has([x - 1, y]) == false) {
-      search(board, word, path, k + 1, x - 1, y);
-    }
-    if (y > 0 && !path.has([x, y - 1]) == false) {
-      search(board, word, path, k + 1, x, y - 1);
-    }
-    path.delete([x, y]);
-  }
-
-  return false;
-};
 let board = [
     ["A", "B", "C", "E"],
     ["S", "F", "C", "S"],
     ["A", "D", "E", "E"],
   ],
-  word = "ABCCED";
+  word = "ABCB";
 let out = exist(board, word);
 console.log(out);
