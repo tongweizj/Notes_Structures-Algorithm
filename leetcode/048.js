9fb2de3aefe1c4c75a70eec0eac824458d9e8251
max <max@luci.com>
2023-05-11 23:35:11 -0400

    vault backup: 2023-05-11 23:35:11

--- leetcode/048.js
+++ leetcode/048.js
@@ -2,6 +2,9 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
+
+// 1. 仔细观察旋转之后，所有数值变换的规律
+//    我在做的时候，只看到了一个，它应该一圈一起观察
 var rotate = function (M) {
   let n = M.length,
     depth = ~~(n / 2);
